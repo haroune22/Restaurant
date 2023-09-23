c445f80056ababfcc7c700910db7aaf99df7b91c
Haroune Hamenniche <harounehamenniche123@gmail.com>
2023-09-23 18:24:18 +0100

    Update route.ts

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server"
 export const GET=async()=>{
     try {
         const categories = await prisma.category.findMany()
-        return new NextResponse(JSON.stringify(categories),{status:500})
+        return new NextResponse(JSON.stringify(categories),{status:200})
     } catch (error) {
         console.log(error)
         return new NextResponse(JSON.stringify({message:'somthing went wrong'}),{status:500})
